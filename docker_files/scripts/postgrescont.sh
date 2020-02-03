ecf7d833db299f09895129af069565ccc9b21907
Yaroksito <ypery@redhat.com>
2020-02-03 03:01:52 -0500

    Remove docker folder

--- docker_files/scripts/postgrescont.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name postgresql --restart="always" -e POSTGRESQL_USER=root -e POSTGRESQL_PASSWORD=redhat -e POSTGRESQL_DATABASE=harbor -e POSTGRESQL_ADMIN_PASSWORD=redhat -p 5432:5432 centos/postgresql-10-centos7
